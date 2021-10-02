8de472858d66ba7f8cf24319c77640e296b729a5
christiansiagiann <christian.andreas@student.umn.ac.id>
2021-10-03 00:37:33 +0700

    3rd commit

---
+++ week07/week07a/app/src/main/java/id/ac/umn/christianandreas/week07a_38106/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.christianandreas.week07a_38106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
